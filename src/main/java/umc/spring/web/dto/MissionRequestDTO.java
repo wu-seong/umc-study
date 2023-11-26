5471dbfe527d5c80c9c0dd13cbef11ff05fb02fd
wuseong <jws001228@naver.com>
2023-11-26 23:59:00 +0900

    FEAT: DTO에 검증 추가

--- src/main/java/umc/spring/web/dto/MissionRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MissionRequestDTO.java
@@ -4,8 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import umc.spring.domain.Store;
 import umc.spring.domain.User;
+import umc.spring.validation.annotation.PassDate;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 public class MissionRequestDTO {
@@ -15,10 +18,13 @@ public class MissionRequestDTO {
 
         private Store store;
 
+        @Positive
         private Integer minimumAmount;
 
+        @Positive
         private Integer rewardPoint;
 
+        @PassDate //근데 찾아보니 날짜 값을 검증하는 @Future과 @Past가 있음..
         private LocalDate deadline;
 
         public void setStore(Store store){
